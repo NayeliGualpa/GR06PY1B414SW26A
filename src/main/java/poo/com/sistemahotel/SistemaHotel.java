11815caf91e5bed5ecc4b6b377b901ebb8aa1d12
Luffytaro22 <manuelsanchez059@gmail.com>
2026-05-20 15:35:48 -0500

    Setup inicial

---
+++ src/main/java/poo/com/sistemahotel/SistemaHotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.com.sistemahotel;
+
+/**
+ *
+ * @author manue
+ */
+public class SistemaHotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
